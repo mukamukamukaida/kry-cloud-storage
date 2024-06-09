c134c2169d20a7113817890fc6346342303af74d
mukamukamukaida <edpnm120@gmail.com>
2024-06-09 21:04:34 +0900

    s3のアップの確認まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
-end
+    has_secure_password
+    has_many :folders
+    has_many :stored_files
+    has_one_attached :avatar
+  
+    def thumbnail
+      avatar.variant(resize: "100x100!").processed
+    end
+  end
+  
